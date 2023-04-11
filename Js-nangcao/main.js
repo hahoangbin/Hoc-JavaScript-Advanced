82b36738cfff6a4a6a07f9afa2c60f014ec0fc40
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-04-11 21:27:37 +0700

    Bài 14 - method overriding

--- Js-nangcao/main.js
+++ Js-nangcao/main.js
@@ -19,26 +19,44 @@
 // const mouse = new Mouse('Mickey')
 // mouse.run()
 
-class Animal {
-  constructor(name) {
-    this.name = name;
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   eat() {
+//   console.log('Eating...!');
+//   }
+// }
+
+// class Bird extends Animal {
+//   fly() {
+//     console.log('Flying...!');
+//   }
+// }
+
+// class Parrot extends Bird {
+//   speak() {
+//     console.log('alo alo...!');
+//   }
+// }
+
+// const bird = new Parrot('Bồ câu')
+// bird.speak()
+
+// method overridings
+class CoffeeMachine {
+  makeCoffee() {
+    console.log('Making Coffee Machine');
   }
-  eat() {
-  console.log('Eating...!');
+}
+class SpecialCoffee extends CoffeeMachine {
+  makeCoffee(cb) {
+    console.log('MakingCoffeeMachine...hihi haha ................');
+    cb()
   }
 }
 
-class Bird extends Animal {
-  fly() {
-    console.log('Flying...!');
-  }
-}
-
-class Parrot extends Bird {
-  speak() {
-    console.log('alo alo...!');
-  }
-}
-
-const bird = new Parrot('Bồ câu')
-bird.speak()
+const Coffee = new SpecialCoffee()
+Coffee.makeCoffee(function() {
+  console.log('call the boss');
+})
