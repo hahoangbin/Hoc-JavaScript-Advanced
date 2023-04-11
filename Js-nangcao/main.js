63c5bf4c1533c8ec774e1815fd2ac75fbbe82677
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-04-11 22:00:52 +0700

    Bài 15 - super

--- Js-nangcao/main.js
+++ Js-nangcao/main.js
@@ -44,19 +44,55 @@
 // bird.speak()
 
 // method overridings
-class CoffeeMachine {
-  makeCoffee() {
-    console.log('Making Coffee Machine');
-  }
-}
-class SpecialCoffee extends CoffeeMachine {
-  makeCoffee(cb) {
-    console.log('MakingCoffeeMachine...hihi haha ................');
-    cb()
+// class CoffeeMachine {
+//   makeCoffee() {
+//     console.log('Making Coffee Machine');
+//   }
+// }
+// class SpecialCoffee extends CoffeeMachine {
+//   makeCoffee(cb) {
+//     console.log('MakingCoffeeMachine...hihi haha ................');
+//     cb()
+//   }
+// }
+
+// const Coffee = new SpecialCoffee()
+// Coffee.makeCoffee(function() {
+//   console.log('call the boss');
+// })
+
+// super
+class Hero {
+  constructor(name, hp, damage) {
+    this.name = name;
+    this.hp = hp;
+    this.damage = damage;
   }
 }
 
-const Coffee = new SpecialCoffee()
-Coffee.makeCoffee(function() {
-  console.log('call the boss');
-})
+applyDamage(damage) {
+  this.hp -= damage
+}
+
+attack(enemy) {
+  enemy.applyDamage(this.damage)
+} 
+
+class RangeHero extends Hero {
+  constructor(name, damage, hp, range) {
+    super(name, damage, hp)
+    this.range = range
+  }
+
+  attack(enemy) {
+    super.attack(enemy)
+    this.hp += this.damage
+  }
+}
+
+const heroAAA = new RangeHero('A', 500, 20)
+const heroBBB = new Hero('B', 1000, 11)
+console.log({heroAAA, heroBBB});
+
+heroAAA.attack(heroBBB)
+console.log({heroAAA, heroBBB});
