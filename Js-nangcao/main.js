d69cb56a6d5351c20f12a2be65d0d63c2d22eea2
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-04-11 21:17:33 +0700

    Bài 13 - class inheritance

--- Js-nangcao/main.js
+++ Js-nangcao/main.js
@@ -7,14 +7,38 @@
 //   console.log(`${this.name} is running}`);
 // }
 
-class Mouse {
+// class Mouse {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   run() {
+//   console.log(`${this.name} is running}`);
+//   }
+// }
+
+// const mouse = new Mouse('Mickey')
+// mouse.run()
+
+class Animal {
   constructor(name) {
     this.name = name;
   }
-  run() {
-  console.log(`${this.name} is running}`);
+  eat() {
+  console.log('Eating...!');
   }
 }
 
-const mouse = new Mouse('Mickey')
-mouse.run()
+class Bird extends Animal {
+  fly() {
+    console.log('Flying...!');
+  }
+}
+
+class Parrot extends Bird {
+  speak() {
+    console.log('alo alo...!');
+  }
+}
+
+const bird = new Parrot('Bồ câu')
+bird.speak()
