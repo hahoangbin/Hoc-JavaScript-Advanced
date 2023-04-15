4860b534f507f0ca77eed24ad52b614e49669ac0
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-04-15 17:05:47 +0700

    Bai 20 spread 2

--- Js-nangcao/main.js
+++ Js-nangcao/main.js
@@ -1,17 +1,34 @@
 
 
-let a1 = 1
-let a2 = 1
-console.log(a1 === a2);
+// let a1 = 1
+// let a2 = 1
+// console.log(a1 === a2);
 
-let obj1 = {a: 1}
-let obj2 = {a: 1}
-console.log(obj1 === obj2);
+// let obj1 = {a: 1}
+// let obj2 = {a: 1}
+// console.log(obj1 === obj2);
 
-let a3 = a2
-a3 = 2
-console.log(a2);
+// let a3 = a2
+// a3 = 2
+// console.log(a2);
 
-let obj3 = obj2
-obj3.a = 2
-console.log(obj2);
+// let obj3 = obj2
+// obj3.a = 2
+// console.log(obj2);
+
+const obj1 = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: {
+    e: 10
+  }
+}
+
+let obj2 = {
+  ...obj1,
+  z: 22
+}
+obj2.d.e = 11
+
+console.log(obj1, obj2);
