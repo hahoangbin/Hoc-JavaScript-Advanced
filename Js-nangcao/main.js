8ebb60d1b8f6ad688f480f0fb61d895b7b419485
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-04-15 16:49:33 +0700

    Bài 19 - Value types vs Reference types

--- Js-nangcao/main.js
+++ Js-nangcao/main.js
@@ -1,150 +1,17 @@
 
-// function Mouse(name) {
-//   this.name = name;
-// }
 
-// Mouse.prototype.run = function() {
-//   console.log(`${this.name} is running}`);
-// }
+let a1 = 1
+let a2 = 1
+console.log(a1 === a2);
 
-// class Mouse {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//   run() {
-//   console.log(`${this.name} is running}`);
-//   }
-// }
+let obj1 = {a: 1}
+let obj2 = {a: 1}
+console.log(obj1 === obj2);
 
-// const mouse = new Mouse('Mickey')
-// mouse.run()
+let a3 = a2
+a3 = 2
+console.log(a2);
 
-// class Animal {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//   eat() {
-//   console.log('Eating...!');
-//   }
-// }
-
-// class Bird extends Animal {
-//   fly() {
-//     console.log('Flying...!');
-//   }
-// }
-
-// class Parrot extends Bird {
-//   speak() {
-//     console.log('alo alo...!');
-//   }
-// }
-
-// const bird = new Parrot('Bồ câu')
-// bird.speak()
-
-// method overridings
-// class CoffeeMachine {
-//   makeCoffee() {
-//     console.log('Making Coffee Machine');
-//   }
-// }
-// class SpecialCoffee extends CoffeeMachine {
-//   makeCoffee(cb) {
-//     console.log('MakingCoffeeMachine...hihi haha ................');
-//     cb()
-//   }
-// }
-
-// const Coffee = new SpecialCoffee()
-// Coffee.makeCoffee(function() {
-//   console.log('call the boss');
-// })
-
-// super
-// class Hero {
-//   constructor(name, hp, damage) {
-//     this.name = name;
-//     this.hp = hp;
-//     this.damage = damage;
-//   }
-// }
-
-// applyDamage(damage); {
-//   this.hp -= damage
-// }
-
-// attack(enemy); {
-//   enemy.applyDamage(this.damage)
-// } 
-
-// class RangeHero extends Hero {
-//   constructor(name, damage, hp, range) {
-//     super(name, damage, hp)
-//     this.range = range
-//   }
-
-//   attack(enemy) {
-//     super.attack(enemy)
-//     this.hp += this.damage
-//   }
-// }
-
-// const heroAAA = new RangeHero('A', 500, 20)
-// const heroBBB = new Hero('B', 1000, 11)
-// console.log({heroAAA, heroBBB});
-
-// heroAAA.attack(heroBBB)
-// console.log({heroAAA, heroBBB});
-
-// class Foo {
-//   static someMethod() {
-//     console.log('someMethod...');
-//   }
-
-//   anotherMethod() {
-//     console.log('another method...');
-//   }
-// }
-// Foo.someMethod()
-
-// function foo() {
-
-// }
-
-// Foo.prototype.anotherMethod = function() {
-
-// }
-
-// Foo.someMethod = function() {
-  
-// }
-
-// Rest 
-// function rest(e, ...rest) {
-//   console.log(e);
-//   console.log(...rest);
-//   console.log(arguments);
-// }
-// rest(2, 3, 6, 45,1, 44)
-
-
-// function sum(...nums) {
-//   return nums.reduce((a, b) => a + b)
-// }
-// sum(1,2 ,3, 4, 5)
-
-// function concat(separator, ...strings) {
-//  return strings.join(separator)
-// }
-// concat('.',  'a', 'b', 'c')
-
-//Spread
-const a = [1, 2, 3, 4, 5]
-const b = [1, ...a, 6]
-console.log(b);
-
-function sum(...numbers) {
-  console.log(numbers);
-}
-sum(...a)
+let obj3 = obj2
+obj3.a = 2
+console.log(obj2);
